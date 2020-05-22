dc51e878cfc67fd04b0add3974bdbcf62a565c79
anlzou <599502931@qq.com>
2020-05-22 17:34:32 +0800

    feat(*07/):add exp7-1.cpp

---
+++ chapters/chapter07-trees-and-binary-trees/code/exp7-1.cpp
@@ -0,0 +1,145 @@
+/***
+ * @Date        : 2020-05-22 16:16:29
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-05-22 16:17:25
+ * @FilePath    :
+ * \data-structure\chapters\chapter07-trees-and-binary-trees\code\exp7-1.cpp
+ * @Describe    : btree
+ */
+#include <iostream>
+#include <malloc.h>
+
+using namespace std;
+#define MaxSize 100
+typedef char ElemType;  //存储类型
+
+typedef struct node {  //定义结点
+    ElemType data;
+    struct node *rchild;
+    struct node *lchild;
+} BTNode;
+
+/**
+ * 创建二叉树
+ * 由括号表示串str创建二链b
+ */
+void CreateBTree(BTNode *&b, char *str) {
+    BTNode *St[MaxSize], *p;
+    int top = -1, k, j = 0;
+    char ch;
+
+    b = NULL;  //创建二叉树初始时为空
+    ch = str[j];
+    while (ch != '\0') {  // str 未扫描完时循环
+        switch (ch) {
+            case '(':  //开始处理左子树
+                top++;
+                St[top] = p;
+                k = 1;
+                break;
+
+            case ')':  //左子树处理完
+                top--;
+                break;
+
+            case ',':  //开始处理右子树
+                k = 2;
+                break;
+
+            default:
+                p = (BTNode *)malloc(sizeof(BTNode));
+                p->data = ch;
+                p->lchild = p->rchild = NULL;
+                if (b == NULL) {  //若b为空，p置为二叉树的根结点
+                    b = p;
+                } else {  //已创建二叉树根结点
+                    switch (k) {
+                        case 1:
+                            St[top]->lchild = p;
+                            break;
+
+                        case 2:
+                            St[top]->rchild = p;
+                            break;
+                    }
+                }
+        }
+        j++;
+        ch = str[j];
+    }
+}
+
+/**
+ *销毁二叉树
+ */
+void DestroyBTree(BTNode *&b) {
+    if (b != NULL) {
+        DestroyBTree(b->lchild);
+        DestroyBTree(b->rchild);
+        free(b);
+    }
+}
+
+/**
+ * 查找值为x的结点
+ */
+BTNode *FindNode(BTNode *b, ElemType x) {
+    BTNode *p;
+    if (b == NULL) {
+        return NULL;
+    } else if (b->data == x) {
+        return b;
+    } else {
+        p = FindNode(b->lchild, x);
+        if (p != NULL) {
+            return p;
+        }
+
+        else {
+            return FindNode(b->rchild, x);
+        }
+    }
+}
+
+/**
+ * 返回p结点的左孩子结点指针
+ */
+BTNode *LchildNode(BTNode *p) { return p->lchild; }
+
+/**
+ * 返回p结点的左孩子结点指针
+ */
+BTNode *RchildNode(BTNode *p) { return p->rchild; }
+
+/**
+ * 求二叉树b的高度
+ */
+int BTHeight(BTNode *b) {
+    int lchildh, rchildh;
+    if (b == NULL) {
+        return 0;
+    } else {
+        lchildh = BTHeight(b->lchild);  //求左子树的高度
+        rchildh = BTHeight(b->rchild);  //求右子树的高度
+        return (lchildh > rchildh) ? (lchildh + 1) : (rchildh + 1);
+    }
+}
+
+/**
+ * 以括号表示法输出二叉树
+ */
+void DispBTree(BTNode *b) {
+    if (b != NULL) {
+        cout << b->data;
+        if (b->lchild != NULL || b->rchild != NULL) {
+            cout << "(";           //有孩子结点时才输出(
+            DispBTree(b->lchild);  //递归处理左子树
+            if (b->rchild != NULL) {
+                cout << ",";  //有右孩子结点时才输出,
+            }
+            DispBTree(b->rchild);  //递归处理右子树
+            cout << ")";           //有孩子结点时才输出)
+        }
+    }
+}
---
+++ chapters/chapter07-trees-and-binary-trees/test-1.md
@@ -0,0 +1,45 @@
+<!--
+ * @Date        : 2020-05-21 17:12:41
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-05-22 17:32:37
+ * @FilePath    : \data-structure\chapters\chapter07-trees-and-binary-trees\test-1.md
+ * @Describe    : 
+--> 
+## 题目地址
+7.1 验证性实验
+
+## 题目描述
+实验题 1：实现二叉树各种基本运算的算法
+
+目的：
+```
+领会二叉链表存储结构和掌握二叉树中各种基本运算算法设计
+```
+目录：
+```java
+CreateBTree(BTNode *&b, char *str)  //由括号表示串str创建二链b
+
+FindNode(BTNode *b, ElemType x)     //返回data域为x的结点指针
+
+RchildNode(BTNode *p)               //返回p结点的左孩子结点指针
+
+LchildNode(BTNode *p)               //返回p结点的右孩子结点指针
+
+BTHeight(BTNode *b)                 //返回二叉树b的高度
+
+DispBTree(BTNode *b)                //以括号表示法输出二叉树b
+
+DestroyBTree(BTNode *&b)            //释放二叉树b的所有结点
+```
+
+
+## 代码
+[exp7-1.cpp](./code/exp7-1.cpp)
+
+[test.cpp](./code/test.cpp)
+
+## 复杂度分析
+
+- 时间复杂度：O( )
+- 空间复杂度：O( )
